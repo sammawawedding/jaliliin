bbf3477b77f199d16ce6174177ec3ef58a4177c9
Muhammad Jamil <mjamil1339@gmail.com>
2021-06-08 18:48:18 +0700

    Remake Wedding Iin & Jalil

--- src/pages/e-ticket.js
+++ src/pages/e-ticket.js
@@ -34,7 +34,7 @@ function ETicket({ location }) {
       }
 
       alert(`Maaf, Kode data ${guestCode} tidak ditemukan. Mohon di cek lagi..`);
-      window.location.assign('https://thekusuma.com');
+      window.location.assign('https://jaliliin.herokuapp.com');
     },
     [data, nameEticket, value],
   );
